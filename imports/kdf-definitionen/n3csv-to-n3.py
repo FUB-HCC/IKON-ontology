60c35bb2ccc85911dfcbb02047dbed49251014fe
Jonas <jonaso@web.de>
2017-12-23 18:22:22 +0100

    created import for kdf definitionen

---
+++ imports/kdf-definitionen/n3csv-to-n3.py
@@ -0,0 +1,19 @@
+import csv
+import sys
+
+outfile = open('kdf-definitionen.n3', 'wb')
+
+n3writer = csv.writer(outfile,
+    delimiter=' ',
+    quotechar='"',
+    escapechar="\\",
+    doublequote=False,
+    quoting=csv.QUOTE_MINIMAL
+)
+
+with open('kdf-definitionen-n3.csv', 'rb') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=' ', quotechar='"')
+    for row in csvreader:
+        row.append(".")
+        n3writer.writerow(row)
+
