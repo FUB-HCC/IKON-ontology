20b04d85b8382880fda1f667772760209d8d4469
Jonas <jonaso@web.de>
2017-12-31 08:35:46 +0100

    documentation with WIDOCO

---
+++ createDocumentation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar ../tools/WIDOCO/jar/widoco-1.4.1-jar-with-dependencies.jar -ontFile ./prod.owl -outFolder ./docs -getOntologyMetadata -oops -rewriteAll -saveConfig ./widocoConfigFile -includeImportedOntologies -htaccess -webVowl -licensius
