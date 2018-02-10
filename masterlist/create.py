dcd79214f959b43d1d0284e5889d98508e41efb9
wittenator <wittenator@yahoo.de>
2018-02-10 17:17:25 +0100

    Fixed SPARQL Query and writing to .csv file

--- masterlist/create.py
+++ masterlist/create.py
@@ -9,8 +9,6 @@ import os
 import glob
 from xlsxwriter.workbook import Workbook
 
-
-
 outputName = "masterlist"
 
 # -------------------------------------------
@@ -18,55 +16,72 @@ outputName = "masterlist"
 csvfileName = "./%s.csv" % outputName
 xlsfileName = "./%s.xlsx" % outputName
 
-
 # kdfs = URIRef("http://ontologies.mfn-berlin.de/ikon")
 
 g = Graph()
 # g.load('https://raw.githubusercontent.com/FUB-HCC/IKON-ontology/master/prod.owl')
 g.parse('../prod.owl')
 
-print 'prod.owl contains %s triples' % len(g)
+print('prod.owl contains %s triples' % len(g))
 
-# rows = []
-# header = []
+rows = []
+header = []
 
-# qres = g.query(
-#    """
-#    SELECT DISTINCT ?s ?type ?parent ?version ?description ?domain ?range
-#    WHERE {
-#        ?s rdf:type ?type .
-#        OPTIONAL {
-#            ?s <http://purl.org/dc/terms/hasVersion> ?version .
-#            ?s <http://purl.org/dc/terms/description> ?description .
-#            ?s rdfs:range ?range .
-#            ?s rdfs:domain ?domain .
-#        }
-#        OPTIONAL {
-#            ?s rdfs:subClassOf ?parent .
-#        }
-#    }
-#    """)
+# einige Attribute in der prod.owl heißen anders als gegeben ()
+
+qres = g.query(
+   """
+   SELECT DISTINCT ?s ?type ?parent ?label ?description ?domain ?range ?version ?status ?notes
+   WHERE {
+       ?s a ?type .
+       
+       OPTIONAL {
+           ?s terms:hasVersion ?version .
+       }
+       OPTIONAL {
+           ?s rdfs:comment ?description .
+       }
+       OPTIONAL {
+           ?s rdfs:range ?range .
+       }
+       OPTIONAL {
+           ?s rdfs:domain ?domain .
+       }
+       OPTIONAL {
+           ?s skos:scopeNote ?notes
+       }
+       OPTIONAL {
+           ?s ns:term_status ?status .
+       }
+       OPTIONAL {
+           ?s rdfs:subClassOf ?parent .
+       }
+       OPTIONAL {
+           ?s rdfs:label ?label .
+           FILTER (lang(?label) = "de")
+       }
+       
+   }
+   """)
 
 
 
 
-#with open(csvfileName, 'wb') as csvfile:
-#    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-#    writer.writerow(['Subject', 'Type', 'Parent', 'Version', 'Description', 'Domain', 'Range', 'Status', 'Notizen'])
-#    for row in qres:
-#        writer.writerow([unicode(r).encode("utf-8") for r in row])
+with open(csvfileName, 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    writer.writerow(['Subject', 'Type', 'Parent', 'Label', 'Description', 'Domain', 'Range', 'Version', 'Status', 'Notizen'])
+    for row in qres:
+        writer.writerow([r if r is not None else '' for r in row])
 
 
 # write Excel file
-with Workbook(xlsfileName) as workbook:
-	worksheet = workbook.add_worksheet()
-	# with open(csvfile, 'rt', encoding='utf8') as f:
-	with open(csvfileName, 'rt') as f:
-	    reader = csv.reader(f)
-	    for r, row in enumerate(reader):
-	        for c, col in enumerate(row):
-	            worksheet.write(r, c, col)
+#with Workbook(xlsfileName) as workbook:
+#	worksheet = workbook.add_worksheet()
+#	# with open(csvfile, 'rt', encoding='utf8') as f:
+#	with open(csvfileName, 'rt') as f:
+#		reader = csv.reader(f)
+#		for r, row in enumerate(reader):
+#			for c, col in enumerate(row):
+#               worksheet.write(r, c, col)
+#
 # workbook.close()
-
-
-
